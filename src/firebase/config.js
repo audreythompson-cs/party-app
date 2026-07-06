a771185d470c53ff666db5e6e3786504c94c4bd9
audrey-thompson <audrey.thompson2@snhu.edu>
2026-07-06 13:54:36 -0500

    fix: hardcode public Firebase client config for cloud build

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBaRgIy_1J4kgeN64URmcQvR6SxxC3v75s",
+  authDomain: "grad-party-2026-85add.firebaseapp.com",
+  projectId: "grad-party-2026-85add",
+  storageBucket: "grad-party-2026-85add.firebasestorage.app",
+  messagingSenderId: "922740580312",
+  appId: "1:922740580312:web:717a068646a1bc28a5dcad"
 };
 
 // Initialize Firebase services
