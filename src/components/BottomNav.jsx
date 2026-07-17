9e46666cd304ebe4dd3aa50882aeaac9b39fc2f3
audrey-thompson <audreythompson.cs@gmail.com>
2026-07-17 17:36:47 -0500

    new points icon

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -30,9 +30,7 @@ export default function BottomNav({ activeTab, setActiveTab, position = 'bottom'
       id: 'points',
       label: 'Points',
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="nav-icon">
-          <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-        </svg>
+        <img src="/points.png" alt="" className="nav-icon custom-nav-icon" />
       )
     },
     {
